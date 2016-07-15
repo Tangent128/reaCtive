a31208b5ff0254abf766860b171ba5434e8086cc
Tangent 128 <Tangent128@gmail.com>
2016-07-15 02:54:03 -0400

    Test canceling downstream.

--- proofofconcept.c
+++ proofofconcept.c
@@ -17,7 +17,10 @@ static void count_init(Observable *context)
     printf("Start counting.\n", i);
     for(i = 0; i < state->max; i++) {
         printf("Emitting %d\n", i);
-        reaC_emit_next(context, i, state->max);
+        if(reaC_emit_next(context, i, state->max)) {
+            /* some error downstream, stop */
+            break;
+        }
     }
     printf("Done counting.\n", i);
     reaC_cancel(context);
@@ -64,6 +67,45 @@ static Observable *new_int_spy()
     return obsv;
 }
 
+/* Demo observable, stops a sequence after a number of results */
+
+struct limit_state {
+    int seen;
+    int max;
+};
+
+static void limit_next(Observable *context, uintptr_t a, uintptr_t b)
+{
+    struct limit_state *state = context->userdata;
+    state->seen++;
+    if(state->seen < state->max) {
+        /* pass along unchanged */
+        reaC_emit_next(context, a, b);
+    } else {
+        reaC_cancel(context);
+    }
+};
+static void limit_dispose(Observable *context)
+{
+    struct limit_state *state = context->userdata;
+    printf("Disposing limiter with max %d.\n", state->max);
+};
+
+static Observable *new_limiter(int max)
+{
+    void *obj = calloc(1, sizeof(Observable) + sizeof(struct limit_state));
+
+    Observable *obsv = obj;
+    struct limit_state *state = obj + sizeof(Observable);
+
+    obsv->next = limit_next;
+    obsv->dispose = limit_dispose;
+    obsv->userdata = state;
+    state->max = max;
+
+    return obj;
+}
+
 /* Main */
 
 int main(int argc, char **argv)
@@ -71,7 +113,7 @@ int main(int argc, char **argv)
     Observable *one = new_count_sequence(5);
     Observable *two = new_int_spy();
     Observable *three = new_int_spy();
-    Observable *four = new_int_spy();
+    Observable *four = new_limiter(3);
     reaC_subscribe(one, two, 0);
     reaC_subscribe(two, three, 0);
     reaC_subscribe(three, four, 0);
